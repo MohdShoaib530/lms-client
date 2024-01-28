0cd70897813b50b538a2ba4b5e32bcf8bc4d57d8
MohdShoaib530 <mohdshoaib91530@gmail.com>
2024-01-28 13:04:54 +0530

    .env work and json.parse fixed

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5006/api/v1";
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 const axiosInstance = axios.create();
 
--- src/pages/Course/AllLectures.jsx
+++ src/pages/Course/AllLectures.jsx
@@ -2,7 +2,6 @@ import { useLocation } from "react-router-dom";
 
 function AllLectues(){
     const {state} = useLocation()
-    console.log('stae',state)
     return (
         <div>{state._id}</div>
     )
