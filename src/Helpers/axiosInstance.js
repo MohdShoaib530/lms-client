563bbd2813e8e8904c94ebdcf574a7d25971a93c
MohdShoaib530 <mohdshoaib91530@gmail.com>
2024-01-28 13:39:28 +0530

    backend url set in .env

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_BASE_URL;
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 const axiosInstance = axios.create();
 
-axiosInstance.defaults.baseURL = BASE_URL;
+axiosInstance.defaults.baseURL = backendUrl;
 axiosInstance.defaults.withCredentials = true;
 
 export default axiosInstance;
